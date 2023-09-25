c411132871018bdb7db8ae7a0d6e962f154587c0
Himanshuba9hel <himanshuba9hel@gmail.com>
2023-09-26 03:24:42 +0530

    add DB

---
+++ app.js
@@ -0,0 +1,15 @@
+const {Level} = require('level');
+
+const db = new Level('db',{valueEncoding: 'json'}); 
+
+// db.put('Movie' , 'Avatar' , function(err){
+//         console.log("you put record in");
+//     }
+// )
+db.get('Movie',function(err,Movie){
+    console.log(Movie);
+})
+
+// db.put('Movie','Tiger', function(err){
+//     console.log("Inseted item");
+// })
