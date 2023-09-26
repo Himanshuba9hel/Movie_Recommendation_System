f708c1c2b450e4b1a5595d2cb421c4e10663eae7
Himanshuba9hel <himanshuba9hel@gmail.com>
2023-09-26 06:22:48 +0530

    protype

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-const {Level} = require('level');
+document.getElementById("").innerHTML ="Home";
 
-const db = new Level('db',{valueEncoding: 'json'}); 
 
-// db.put('Movie' , 'Avatar' , function(err){
-//         console.log("you put record in");
-//     }
-// )
-db.get('Movie',function(err,Movie){
-    console.log(Movie);
-})
 
-// db.put('Movie','Tiger', function(err){
-//     console.log("Inseted item");
+
+
+
+
+// const {Level} = require('level');
+
+// const db = new Level('db',{valueEncoding: 'json'}); 
+
+// // db.put('Movie' , 'Avatar' , function(err){
+// //         console.log("you put record in");
+// //     }
+// // )
+// db.get('Movie',function(err,Movie){
+//     console.log(Movie);
 // })
+
+// // db.put('Movie','Tiger', function(err){
+// //     console.log("Inseted item");
+// // })
